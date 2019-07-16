ef39e8c60bc277b536e9c63cfeab38f90930894f
Joe Cera <subscribeto@runbox.com>
2019-07-16 16:58:10 -0700

    creating FilePost model for initial testing

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class FilePost(models.Model):
+    username = models.CharField(max_length=30)
+    text = models.TextField()
+    expiry = models.CharField(max_length=5)
+    filename = models.CharField(max_length=150)
